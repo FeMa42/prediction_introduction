e3ce20132f150fdc6e6e723ce3f65a44132effa1
Damian Boborzi <damian.boborzi@etas.com>
2020-01-13 20:00:29 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(
+    name="prediction_introduction",
+    version="0.1",
+    include_package_data=True,
+    packages=[
+        "prediction_introduction",
+    ]
+)
